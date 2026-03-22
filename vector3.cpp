040c028cd33290ecafeb18ffb7d57f35de3dfa34
Jitin Kumar <jk9120810343@gmail.com>
2026-03-22 23:52:50 +0530

    Correct loop condition for populating squares vector
    Fix the loop condition to prevent out-of-bounds access.
    
    Signed-off-by: Jitin Kumar <jk9120810343@gmail.com>

--- vector3.cpp
+++ vector3.cpp
@@ -1,7 +1,6 @@
 #include<iostream>
 #include<vector>
 using namespace std;
-//storing squares from 1 to 100 in a vector
 int main(){
     vector<int>squares(100);
     for(int i=1;i<=squares.size();i++){
